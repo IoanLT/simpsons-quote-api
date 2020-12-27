9c5f245d7a005755ca56bc40e3404e2fd4c557dd
IoanLT <ioan.tranole@gmail.com>
2020-12-27 13:14:06 +0000

    completed api request from componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import QuoteCard from './components/QuoteCard';
 
-const initialQuote = [
-  {
-    "quote": "By chilling my loins I increase the chances of impregnating my wife.",
-    "character": "Apu Nahasapeemapetilon",
-    "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
-    "characterDirection": "Left"
-  }
-];
+// const initialQuote = [
+//   {
+//     "quote": "By chilling my loins I increase the chances of impregnating my wife.",
+//     "character": "Apu Nahasapeemapetilon",
+//     "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+//     "characterDirection": "Left"
+//   }
+// ];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: initialQuote
+      quote: null
     };
+  }
+
+  componentDidMount() {
+    this.getQuote();
   }
 
   getQuote = () => {
@@ -38,7 +42,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <QuoteCard quote={this.state.quote} />
+        {
+          this.state.quote 
+            ?  <QuoteCard quote={this.state.quote} /> 
+            :  <p>Loading..</p>
+        }
+        
         <button type="button" onClick={this.getQuote}>Get quote</button>
       </div>
     );
